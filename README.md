70c1ce8ddb07b2c7c24f1475496d37b6a9121c55
Richard Hoehn <rhoehn@gmail.com>
2023-03-22 15:04:41 -0500

    Adding S3 Upload and Head Retrieval

--- README.md
+++ README.md
@@ -2,12 +2,17 @@
 
 Welcome to the MTSU CSCI 7400 - Assignment 02 demo.
 
+The goal of this assignment is to learn about AWS cloud services. You should explore at least one AWS service and create a short video (10-15 minutes) about the selected service.
+
+The video should give a brief description about the service – what it is, why it is important, what are its use cases. The video should also include a small example use case using the selected AWS service. It is expected that a PowerPoint presentation is used to explain the contents of the video.
+
 ## ENV Setup
 
 I did not publish the `.env` located in the root of hte `/src/` folder. The following should be the structure of the `.env` files:
 
 ```
 AWS_REGION="us-east-1"
+AWS_BUCKET=
 AWS_ACCESS_KEY_ID=
 AWS_SECRET_ACCESS_KEY=
 AWS_SESSION_TOKEN=
--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@
 // By:          Richard Hoehn (PhD Candiate)
 // Pub Date:    2023-03-25
 // Due Date:    2023-04-06
-// Desc:        This is a project to introduct & demonstate AWS Polly for Assignment 02
+// Desc:        This is a project to introduct & demonstate AWS Polly and S3 for Assignment 02. This application
+//              requests a user's name and creates a simple text to speech command that gets syntezied by AWS Polly (cloud)
+//              and returned as a Readable Stream to the application. It then saves the file (mp3) to local disk and also
+//              uploads to a spesfic S3-bucket on AWS S3. And finally, for demonstration purposes, plays the mp3 file on 
+//              the PC's speakers as an added bonus.
 // Links:       * AWS Polly: https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-pollyClient/
 //              * AWS S3:    https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-s3/
 //              * NodeJS:    https://nodejs.org/en/docs
@@ -28,6 +32,7 @@ const player = require("sound-play");
 
 // AWS Polly Module
 const { PollyClient, SynthesizeSpeechCommand } = require('@aws-sdk/client-polly');
+const { S3Client, PutObjectCommand, HeadObjectCommand } = require("@aws-sdk/client-s3");
 
 // Input from Terminal Control
 const readline = require('node:readline/promises');
@@ -39,6 +44,7 @@ const rl = readline.createInterface({ input, output });
 const run = async () => {
     console.clear();
     const pollyClient = new PollyClient();
+    const s3Client = new S3Client();
 
     // Get User's name... and setup date & time variables
     const userName = await rl.question("\n\nHi,\nWhat's your name: ");
@@ -55,8 +61,13 @@ const run = async () => {
         VoiceId: 'Ruth',
     };
 
+    const s3Params = {
+        Bucket: process.env.AWS_BUCKET,
+        Key: filename
+    };
+
     // Send to AWS for Speech Syntesis
-    console.log(`\nSending to AWS Polly:\n\n>>> ${pollyParams.Text}\n`);
+    console.log(`\nSending to AWS Polly:\n>>> ${pollyParams.Text} <<<`);
     console.time('AWS Round Trip');
     const data = await pollyClient.send(new SynthesizeSpeechCommand(pollyParams));
     console.timeEnd('AWS Round Trip');
@@ -64,10 +75,29 @@ const run = async () => {
     // If the response is of type "Readable" the stream to local disk and play
     if (data.AudioStream instanceof Readable) {
 
+        console.log(`\nSaving (${filename}) to local disk now...`);
+        console.time('Local Save');
         await pipeline(data.AudioStream, fs.createWriteStream(`./media/${filename}`));
+        console.log('Local save complete');
+        console.timeEnd('Local Save');
 
-        console.log('\nPlaying returned "mp3" now...\n');
+        console.log(`\nUploading (${filename}) to S3 now...`);
+        console.time('AWS Round Trip');
+        const mp3File = fs.readFileSync(`./media/${filename}`);
+        await s3Client.send(new PutObjectCommand({Body: mp3File, ...s3Params}));
+        console.log('Uploading to S3 complete');
+        console.timeEnd('AWS Round Trip');
+
+        console.log('\nGet size of uploaded file from S3 now..');
+        console.time('AWS Round Trip');
+        const s3Resp = await s3Client.send(new HeadObjectCommand(s3Params));
+        console.log(`File (${filename}) retrieved is ${(s3Resp.ContentLength/1024).toFixed(2)}kb`);
+        console.timeEnd('AWS Round Trip');
+
+        console.log('\nPlaying returned "mp3" now...');
+        console.time('Sound Play Time');
         await player.play(`./media/${filename}`);
+        console.timeEnd('Sound Play Time');
     }
 
     console.log('\nDone with Demo - Have a good day!\n\n\n');
